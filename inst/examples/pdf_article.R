991c8461433982ec5f6c04639292ed5d69a9f7bd
Wenjie Wang <wang@wwenjie.org>
2021-09-08 00:15:07 -0400

    Update Rd and fix a typo

--- inst/examples/pdf_article.R
+++ inst/examples/pdf_article.R
@@ -1,6 +1,6 @@
 if (interactive() && requireNamespace("rmarkdown", quietly = TRUE)) {
     ## draft from a sample R markdown file
-    jds::draft("jds-sample.Rmd")
+    jds.rmd::draft("jds-sample.Rmd")
     ## produce pdf with the tex source kept for submission
     rmarkdown::render("jds-sample.Rmd")
 }
